de9970595ea59622fb1d549b6bafa69c9cf3b9c0
saraivais <isadorasaraiva95@gmail.com>
2022-03-16 17:04:21 -0300

    Done: requisito 4~ mentoria eh vida! #meSoltaLint 🦔

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+// console.log(employees);
+
 function isManager(id) {
-  // seu código aqui
+  // retorna true se esse id estiver contido em [managers] de algum outro funcionário;
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const underManager = employees
+      .filter(({ managers }) => managers.includes(managerId));
+    const employeeName = underManager.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+    return employeeName;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+
 }
 
 module.exports = { isManager, getRelatedEmployees };
