cd59dca4f56323a9e917959e9659f06f0e64c59a
saraivais <isadorasaraiva95@gmail.com>
2022-03-16 16:00:42 -0300

    Done: requisito 2~ looks like i got found my mojo back huh 🦈

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+const { species } = data;
+// console.log(species);
+
+function getAnimalsOlderThan(animalName, age) {
+  // pegar o objeto pelo name, que é uma string
+  // avaliar o array de objetos residents, por age, se é maior que o parametro age;
+  const chosenAnimal = species.find((animal) => animal.name === animalName);
+  const { residents } = chosenAnimal;
+  return residents.every((lilAnimal) => lilAnimal.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
