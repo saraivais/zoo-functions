8b9dfacd22bb1a688ef00ccb057f4a554a67cdc4
saraivais <isadorasaraiva95@gmail.com>
2022-03-16 18:05:37 -0300

    Done: requisito 5~ I feel like this 🐮 but its ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,19 +1,32 @@
 const data = require('../data/zoo_data');
 
 const { species } = data;
-// console.log(species);
+
+const returnAllAnimals = () => {
+  const obj = {};
+  species.forEach((eachAnimal) => {
+    obj[eachAnimal.name] = eachAnimal.residents.length;
+  });
+  return obj;
+};
 
 function countAnimals(animal) {
-  // se !animal, retorna um objeto com entries ('stringNomeAnimal' : quantidadeAnimals);
-  const obj = {};
   if (!animal) {
-    species.forEach((eachAnimal) => {
-      obj[eachAnimal.name] = eachAnimal.residents.length;
-    });
-    return obj;
+    return returnAllAnimals();
   }
+
+  const selectedSpecie = animal.specie;
+  const selectedSpecies = species.find((specie) => specie.name === selectedSpecie);
+  const { residents } = selectedSpecies;
+
+  if (animal.sex) {
+    const selectedSex = animal.sex;
+    const filteredResidents = residents.filter((eachAnimal) => eachAnimal.sex === selectedSex);
+    return filteredResidents.length;
+  }
+  return residents.length;
 }
 
-console.log(countAnimals());
+console.log(countAnimals({ specie: 'lions' }));
 
 module.exports = countAnimals;
