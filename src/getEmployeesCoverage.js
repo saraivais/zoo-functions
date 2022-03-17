5540f5f2535b9276d3319a4bf00123d3d937927c
saraivais <isadorasaraiva95@gmail.com>
2022-03-17 14:55:17 -0300

    Internet voltou~ 🐧🐸🦁

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+console.log(species, employees);
+// recebe nome ou id, dentro de um objeto~
+// cata o employee;
+// tem que devolver o employee com id, fullname (name + lastname), species (responsiblefor - pega nome com id~), locations (responsiblefor - pega location com id~);
+
+function getEveryEmployee() {
+
+}
+
+function getOneEmployee() {
+
+}
+
+// sem parâmetro ~ retorna tudo de todo mundo~
+function getEmployeesCoverage(chosenEmployee) {
+  if (!chosenEmployee) {
+    getEveryEmployee();
+  } else if (chosenEmployee.id) {
+    getOneEmployee();
+  } else if (chosenEmployee.name) {
+    getOneEmployee();
+  }
 }
 
 module.exports = getEmployeesCoverage;
