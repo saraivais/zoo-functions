86f45ef088b4478e46affbd59df7c629de9d082f
saraivais <isadorasaraiva95@gmail.com>
2022-03-17 13:31:39 -0300

    Sem internet~ Done: requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+// console.log(employees);
+// console.log(species);
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // receber Id do funcionário, vou pegar o responsible for~ primeiro;
+  const thisEmployee = employees.find((employee) => employee.id === id);
+  // console.log(thisEmployee);
+  const idFirstAnimal = thisEmployee.responsibleFor[0];
+  // console.log(idFirstAnimal);
+  const firstAnimal = species.find((animal) => animal.id === idFirstAnimal);
+  // console.log(firstAnimal);
+  const animalResidents = firstAnimal.residents;
+
+  const oldestAnimal = animalResidents.reduce((acc, animal) => acc.age > animal.age ? acc : animal);
+  return Object.values(oldestAnimal);
+
+  // pegar o animal com o id que eu tenho;
+  // dps fazer um reduce pra pegar o animal mais velho~ 
+
+
 }
+
+getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 
 module.exports = getOldestFromFirstSpecies;
