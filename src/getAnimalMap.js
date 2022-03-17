f1c3a3233dc5a85ae6cb36752a667fc521cbd835
saraivais <isadorasaraiva95@gmail.com>
2022-03-17 18:01:57 -0300

    Done: requisito 10~ 80% eh o novo 100 🐿

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -16,29 +16,63 @@ const data = require('../data/zoo_data');
 //   return objectToReturn;
 // }
 
-function getAnimalMap(options) {
-}
+// function mapWithNamesNoFilter() {
+//   const objectToReturn = {};
+//   species.forEach(({ name, location, residents }) => {
+//     const allNames = residents.map((animalzinho) => animalzinho.name);
+//     const obj = {
+//       [name]: allNames,
+//     };
+//     // console.log(name, location);
+//     if (!objectToReturn[location]) {
+//       objectToReturn[location] = [];
+//       objectToReturn[location].push(obj);
+//     } else {
+//       objectToReturn[location].push(obj);
+//     }
+//   });
+//   return objectToReturn;
+// }
+
+// function mapWithNamesAndSex() {
+//   const objectToReturn = {};
+//   species.forEach(({ name, location, residents }) => {
+//     const allNames = residents.map((animalzinho) => animalzinho.name);
+//     const obj = {
+//       [name]: allNames,
+//     };
+//     // console.log(name, location);
+//     if (!objectToReturn[location]) {
+//       objectToReturn[location] = [];
+//       objectToReturn[location].push(obj);
+//     } else {
+//       objectToReturn[location].push(obj);
+//     }
+//   });
+//   return objectToReturn;
+// }
+
+// tranformar a string em um objeto, com a string sendo chave e o valor sendo um array com o nome da galera;
+
 // !options ~ retorna nomes das espécies por location, dentro de um array que é o value;
 // !includeNames ~ mesma coisa de !options
 // com includeNames ~ pega o name dos resudents, um array pra cada animal;
-// const objectToReturn = {};
-// if (!options || !options.includeNames) {
-//   return generalMap();
-// }
-// if (options.includeNames === true) {
-//   // criadas chaves do objeto com as localizaçoes
-//   species.forEach(({ name, location, residents }) => {
-//     if (!objectToReturn[location]) {
-//       objectToReturn[location] = [];
-//     }
-//     if (!objectToReturn[location].includes(name)) {
-//       objectToReturn[location].push({[name]: []});
-//     }
-//   });
-// }
+
+function getAnimalMap(options) {
+  // if (!options || !options.includeNames) {
+  //   return generalMap();
+  // }
+  // if (options.includeNames === true && !options.sex) {
+  //   return mapWithNamesNoFilter();
+  // }
+  // if (options.includeNames && options.sex) {
+  //   mapWithNamesAndSex();
+  // }
+}
 
 // return objectToReturn;
 
+console.log(getAnimalMap());
 console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,28 +1,59 @@
 const data = require('../data/zoo_data');
 
 const { species, employees } = data;
-console.log(species, employees);
+// console.log(species, employees);
 // recebe nome ou id, dentro de um objeto~
 // cata o employee;
 // tem que devolver o employee com id, fullname (name + lastname), species (responsiblefor - pega nome com id~), locations (responsiblefor - pega location com id~);
 
+// new Set()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set;
 function getEveryEmployee() {
-
+  const allEmployees = employees.map((eachEmployee) => {
+    const newObj = {
+      id: eachEmployee.id,
+      fullName: `${eachEmployee.firstName} ${eachEmployee.lastName}`,
+      species: eachEmployee.responsibleFor.map((id) => species
+        .find((animal) => animal.id === id).name),
+      locations: eachEmployee.responsibleFor.map((id) => species
+        .find((animal) => animal.id === id).location),
+    };
+    return newObj;
+  });
+  return allEmployees;
 }
 
-function getOneEmployee() {
-
+function getOneEmployee(nameOrId) {
+  const thisEmployee = employees
+    .find((eachEmployee) => eachEmployee.firstName === nameOrId
+    || eachEmployee.lastName === nameOrId || eachEmployee.id === nameOrId);
+  if (!thisEmployee) {
+    throw new Error('Informações inválidas');
+  }
+  const newObj = {
+    id: thisEmployee.id,
+    fullName: `${thisEmployee.firstName} ${thisEmployee.lastName}`,
+    species: thisEmployee.responsibleFor.map((id) => species
+      .find((animal) => animal.id === id).name),
+    locations: thisEmployee.responsibleFor.map((id) => species
+      .find((animal) => animal.id === id).location),
+  };
+  return newObj;
 }
 
 // sem parâmetro ~ retorna tudo de todo mundo~
 function getEmployeesCoverage(chosenEmployee) {
   if (!chosenEmployee) {
-    getEveryEmployee();
-  } else if (chosenEmployee.id) {
-    getOneEmployee();
-  } else if (chosenEmployee.name) {
-    getOneEmployee();
+    return getEveryEmployee();
+  }
+  if (chosenEmployee.id) {
+    return getOneEmployee(chosenEmployee.id);
+  }
+  if (chosenEmployee.name) {
+    return getOneEmployee(chosenEmployee.name);
   }
 }
+
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
