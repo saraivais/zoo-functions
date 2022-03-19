781b6900478d08ae4b558266cdacd4aa22227f74
saraivais <isadorasaraiva95@gmail.com>
2022-03-19 20:09:05 -0300

    Chore: requisito 8~ 🐙 Comentarios e organizaçao do codigo 🐫

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,15 +1,18 @@
 const data = require('../data/zoo_data');
 
 const { species, hours } = data;
+
+// Foram criados arrays de strings contendo os dias da semana e os nomes dos animais, para futura comparação de parâmetros;
 const weekDays = Object.keys(hours);
-// console.log(weekDays);
 const animalNames = species.map((animal) => animal.name);
 
+// Esta função recebe o nome de um animal e retorna um array de strings com os dias nos quais este animal está disponível.
 function animalSchedule(animalName) {
   const selectedAnimal = species.find((animal) => animal.name === animalName);
   return selectedAnimal.availability;
 }
 
+// Esta função não recebe parâmetros e retorna um objeto padronizado, específico para as segundas.
 function itsMonday() {
   const wholeSchedule = {
     Monday: {
@@ -20,6 +23,9 @@ function itsMonday() {
   return wholeSchedule;
 }
 
+// Esta função recebe o nome de um dia da semana e retorna um objeto contendo o schedule daquele dia.
+// Se o dia for 'Monday', chama e retorna a função itsMonday().
+// Para os outros dias, retorna um objeto contento como key o nome do dia e como valor, outro objeto, com as keys 'officeHour' e 'exhibition', que contém o horário de funcionamento (que é uma string) e um array contendo os nomes dos animais disponíveis, respectivamente, como seus valores.
 function daySchedule(dayName) {
   if (dayName === 'Monday') {
     return itsMonday();
@@ -38,14 +44,9 @@ function daySchedule(dayName) {
   return wholeSchedule;
 }
 
-// const entireSchedule = weekDays.map((day) => getSchedule(day));
-// const newObject = entireSchedule.reduce(((acc, curr, index) => {
-//   const [value] = Object.values(curr);
-//   acc[Object.keys(curr)] = value;
-//   return acc;
-// }), {});
-// return newObject;
-
+// Esta função recebe como parâmetro uma string, que pode ser um dia da semana, o nome de um animal ou qualquer coisa. A função chama outras funções específicas de acordo com o parâmetro passado.
+// Se o parâmetro estiver contido no array de strings de dias da semana, será chamada a função daySchedule, que retorna o schedule do dia. Se o parâmetro estiver contido no array de strings de nomes de animais, será chamada a função animalSchedule, que retorna os dias nos quais o animal estará disponível.
+// Se o parâmetro passado não estiver contido em nenhum dos dois arrays, a função retorna um objeto, cujas chaves são dias da semana e os valores, os respectivos schedules destes dias. A criação deste objeto é feita de maneira recursiva, utilizando o array weekDays declarado acima para gerar o schedule de cada dia, concatenando os resultados dentro de um objeto utilizando reduce.
 function getSchedule(scheduleTarget) {
   if (weekDays.includes(scheduleTarget)) {
     return daySchedule(scheduleTarget);
@@ -62,5 +63,15 @@ function getSchedule(scheduleTarget) {
   }), {});
   return entireSchedule;
 }
+
+/*
+const entireSchedule = weekDays.map((day) => getSchedule(day));
+const newObject = entireSchedule.reduce(((acc, curr, index) => {
+  const [value] = Object.values(curr);
+  acc[Object.keys(curr)] = value;
+  return acc;
+}), {});
+return newObject;
+*/
 
 module.exports = getSchedule;
