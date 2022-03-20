80946853e65e5a3d76dc2fda1b91b5b09bdb1031
saraivais <isadorasaraiva95@gmail.com>
2022-03-20 02:28:53 -0300

    Chore: requisito 2~ 🐱 Comentarios e organizaçao de codigo 🦁

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,11 +1,11 @@
 const data = require('../data/zoo_data');
 
 const { species } = data;
-// console.log(species);
 
+// Esta função recebe como parâmetros uma string, que corresponde ao nome de um animal, e um número, que corresponde a uma idade.
+// O objetivo da função é verificar se todos os animais da espécie indicada pelo parâmetro animalName têm idade superior ao número passado pelo parâmetro age.
+// O retorno desta função é um valor booleano, sendo true para todos os animais mais velhos que 'age' e false para o contrário.
 function getAnimalsOlderThan(animalName, age) {
-  // pegar o objeto pelo name, que é uma string
-  // avaliar o array de objetos residents, por age, se é maior que o parametro age;
   const chosenAnimal = species.find((animal) => animal.name === animalName);
   const { residents } = chosenAnimal;
   return residents.every((lilAnimal) => lilAnimal.age > age);
