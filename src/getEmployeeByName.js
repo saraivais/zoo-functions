ab67efc651c4ee8c69a50f50b5e464a14da9b934
saraivais <isadorasaraiva95@gmail.com>
2022-03-20 02:25:24 -0300

    Chore: requisito 3~ 🐹 Comentarios e organizaçao do codigo 🐰

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,12 +2,14 @@ const data = require('../data/zoo_data');
 
 const { employees } = data;
 
+// Esta função recebe como parâmetro uma string e tem como objetivo buscar o objeto de um funcionário pelo seu nome, firstName ou lastName, e retorná-lo.
+// Se a função não recebe parâmetros, deve retornar um objeto vazio.
 function getEmployeeByName(employeeName) {
   if (!employeeName) {
     return {};
   }
-  const pickedEmployee = employees.find((person) => person.firstName === employeeName
-  || person.lastName === employeeName);
+  const pickedEmployee = employees
+    .find((person) => person.firstName === employeeName || person.lastName === employeeName);
   return pickedEmployee;
 }
 
