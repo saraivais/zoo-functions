fb436c1e8c67942ca1ae0d9e3111cd1f804bdb59
saraivais <isadorasaraiva95@gmail.com>
2022-03-20 02:35:36 -0300

    Chore & Refactor: requisito 1~ 🦊 Troca de hof, comentario e organizaçao do codigo 🐨

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,20 +1,20 @@
 const data = require('../data/zoo_data');
 
-// console.log(Object.keys(data));
-// const { species, employees, hours, prices } = data;
-// console.log(species, employees, hours, prices);
-// console.log(species);
 const { species } = data;
-// console.log(species);
-// console.log(species[0].id);
 
-// Tem que receber parâmetro rest, para poder receber mais de um Id;
+// Esta função recebe como parâmetro strings que representam o id dos animais a serem buscados. A quantidade de parâmetros pode variar, por isso o uso do parâmetro rest.
+// A função getSpeciesByIds retorna um array. O array pode ser vazio, caso não seja recebido nenhum parâmetro, ou contendo objetos correspondentes a cada animal cujo id foi recebido.
 function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.find((animal) => animal.id === id));
+}
+
+// Código antigo:
+/*
   const chosenSpecies = [];
   ids.forEach((id) => {
     chosenSpecies.push(species.find((animal) => animal.id === id));
   });
   return chosenSpecies;
-}
+*/
 
 module.exports = getSpeciesByIds;
